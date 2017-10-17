84cfa8954f5b25c33162dd2c8fdcd704db681547
Paul Cunnane <paul@cunnane.net>
2017-10-17 14:56:31 +0100

    eschava fix

--- edge-trigger/hysteresis.js
+++ edge-trigger/hysteresis.js
@@ -7,18 +7,23 @@ module.exports = function(RED) {
                 var this_value = Number(msg.payload);
                 if (! isNaN(this_value)) {
                     var last_value = Number(node.last_value);
+                    var last_edge = node.last_edge;
                     if (! isNaN(last_value)) {
                         if (this_value > last_value &&
                             this_value > config.rising_threshold &&
-                            last_value <= config.rising_threshold) {
+                            last_value <= config.rising_threshold &&
+                            last_edge != 'rising') {
                                 msg.edge = 'rising';
                                 node.send(msg);
+                                node.last_edge = 'rising';
                         }
                         if (this_value < last_value &&
                             this_value < config.falling_threshold &&
-                            last_value >= config.falling_threshold) {
+                            last_value >= config.falling_threshold &&
+                            last_edge != 'falling') {
                                 msg.edge = 'falling';
                                 node.send(msg);
+                                node.last_edge = 'falling';
                         }
                     }
                     node.last_value = this_value;
