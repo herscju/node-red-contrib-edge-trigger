95f6ba5302e1f7b54609cd48835d235dda3818e7
Eugene Schava <eschava@gmail.com>
2020-11-30 13:26:45 +0200

    v1.0.0 Added status to hysteresis node.

--- edge-trigger/hysteresis.js
+++ edge-trigger/hysteresis.js
@@ -2,6 +2,8 @@ module.exports = function(RED) {
     function HysteresisNode(config) {
         RED.nodes.createNode(this, config);
         var node = this;
+        node.status({fill:"grey", shape:"dot", text:"unknown"});
+
         this.on('input', function(msg) {
             if (msg.hasOwnProperty('payload')) {
                 var this_value = Number(msg.payload);
@@ -13,33 +15,35 @@ module.exports = function(RED) {
                             this_value > config.rising_threshold &&
                             last_value <= config.rising_threshold &&
                             last_edge != 'rising') {
-                                msg.edge = 'rising';
-                                node.send(msg);
-                                node.last_edge = 'rising';
+                                status(msg, true);
                         }
                         if (this_value < last_value &&
                             this_value < config.falling_threshold &&
                             last_value >= config.falling_threshold &&
                             last_edge != 'falling') {
-                                msg.edge = 'falling';
-                                node.send(msg);
-                                node.last_edge = 'falling';
+                                status(msg, false);
                         }
                     } else { // first value
                         if ((config.initial_edge == 'any' || config.initial_edge == 'rising') && this_value > config.rising_threshold) {
-                            msg.edge = 'rising';
-                            node.send(msg);
-                            node.last_edge = 'rising';
+                            status(msg, true);
                         } else if ((config.initial_edge == 'any' || config.initial_edge == 'falling') && this_value < config.falling_threshold) {
-                            msg.edge = 'falling';
-                            node.send(msg);
-                            node.last_edge = 'falling';
+                            status(msg, false);
                         }
                     }
                     node.last_value = this_value;
                 }
             }
         });
+
+        function status(msg, rising) {
+            var edge = rising ? 'rising' : 'falling';
+
+            msg.edge = edge;
+            node.send(msg);
+
+            node.last_edge = edge;
+            node.status({fill:rising ? "green" : "yellow", shape:"dot", text:edge});
+        }
     }
     RED.nodes.registerType('hysteresis', HysteresisNode);
 }
